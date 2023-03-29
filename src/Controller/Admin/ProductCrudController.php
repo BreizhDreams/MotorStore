af9379abc67afd4f1f5dc209115d3277adbf0b70
BreizhDreams <Frandu22600@hotmail.fr>
2023-03-29 18:25:33 +0200

    [#18] Image Folder Correction and Update of HTML Title Page

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -6,6 +6,7 @@ use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -27,7 +28,7 @@ class ProductCrudController extends AbstractCrudController
             TextField::new('details', 'Détails'),
             SlugField::new('slug')->setTargetFieldName('designation'),
             MoneyField::new('prixTTC', 'Prix en €')->setCurrency('EUR'),
-            TextField::new('photoURL','Lien de la Photo'),
+            ImageField::new('photoURL','Lien de la Photo')->setUploadedFileNamePattern('[slug]-[timestamp].[extension]')->setBasePath('uploads/')->setUploadDir('public/uploads/'),
             AssociationField::new('categoryVO','Catégorie'),
             AssociationField::new('subCategoryVO','Sous-Catégorie'),
 
